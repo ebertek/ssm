531866cd151c6186e4c647e10327a2bcbb6327b2
David Ebert <ebertek@gmail.com>
2025-07-28 18:36:18 +0200

    Add more UV index locations
    Fixes #4

--- custom_components/ssm/const.py
+++ custom_components/ssm/const.py
@@ -45,10 +45,22 @@ STATIONS = [
 # UV index measurement locations with their API names and values
 LOCATIONS = [
     {
+        "id": "sverige-abisko",
+        "name": "Abisko",
+        "api_name": "Sverige (Abisko)",
+        "latitude": "68.4",
+    },
+    {
         "id": "sverige-gotland",
         "name": "Gotland",
         "api_name": "Sverige (Gotland)",
         "latitude": "57.621875",
+    },
+    {
+        "id": "sverige-gavle",
+        "name": "Gävle",
+        "api_name": "Sverige (Gävle)",
+        "latitude": "60.8",
     },
     {
         "id": "sverige-goteborg",
@@ -57,10 +69,46 @@ LOCATIONS = [
         "latitude": "57.70887",
     },
     {
+        "id": "sverige-halmstad",
+        "name": "Halmstad",
+        "api_name": "Sverige (Halmstad)",
+        "latitude": "56.8",
+    },
+    {
+        "id": "sverige-idre-fjall",
+        "name": "Idre Fjäll",
+        "api_name": "Sverige (Idre Fjäll)",
+        "latitude": "62",
+    },
+    {
+        "id": "sverige-jonkoping",
+        "name": "Jönköping",
+        "api_name": "Sverige (Jönköping)",
+        "latitude": "57.6",
+    },
+    {
+        "id": "sverige-karlstad",
+        "name": "Karlstad",
+        "api_name": "Sverige (Karlstad)",
+        "latitude": "59.2",
+    },
+    {
+        "id": "sverige-kebnekaise",
+        "name": "Kebnekaise",
+        "api_name": "Sverige (Kebnekaise)",
+        "latitude": "68",
+    },
+    {
         "id": "sverige-malmo",
         "name": "Malmö",
         "api_name": "Sverige (Malmö)",
         "latitude": "55.60498",
+    },
+    {
+        "id": "sverige-mora",
+        "name": "Mora",
+        "api_name": "Sverige (Mora)",
+        "latitude": "61.2",
     },
     {
         "id": "sverige-polcirkeln",
@@ -69,16 +117,58 @@ LOCATIONS = [
         "latitude": "66.54772",
     },
     {
+        "id": "sverige-riksgransen",
+        "name": "Riksgränsen",
+        "api_name": "Sverige (Riksgränsen)",
+        "latitude": "68.4",
+    },
+    {
         "id": "sverige-stockholm",
         "name": "Stockholm",
         "api_name": "Sverige (Stockholm)",
         "latitude": "59.32893",
     },
     {
+        "id": "sverige-sundsvall",
+        "name": "Sundsvall",
+        "api_name": "Sverige (Sundsvall)",
+        "latitude": "62.4",
+    },
+    {
+        "id": "sverige-salen",
+        "name": "Sälen",
+        "api_name": "Sverige (Sälen)",
+        "latitude": "61.2",
+    },
+    {
+        "id": "sverige-tanndalen",
+        "name": "Tänndalen",
+        "api_name": "Sverige (Tänndalen)",
+        "latitude": "62.4",
+    },
+    {
+        "id": "sverige-umea",
+        "name": "Umeå",
+        "api_name": "Sverige (Umeå)",
+        "latitude": "64",
+    },
+    {
+        "id": "sverige-vemdalen",
+        "name": "Vemdalen",
+        "api_name": "Sverige (Vemdalen)",
+        "latitude": "62.4",
+    },
+    {
         "id": "sverige-oland",
         "name": "Öland",
         "api_name": "Sverige (Öland)",
         "latitude": "58.866991",
+    },
+    {
+        "id": "sverige-orebro",
+        "name": "Örebro",
+        "api_name": "Sverige (Örebro)",
+        "latitude": "59.2",
     },
     {
         "id": "sverige-ostersund",
